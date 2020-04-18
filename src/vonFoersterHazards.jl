7bb6f2c683b5d96e6c54c09103593f281d08be39
Aaron Sheldon <aaronsheldon@hushmail.com>
2020-04-18 10:51:46 -0600

    revise covariance calculation to use index mangling #5

--- src/vonFoersterHazards.jl
+++ src/vonFoersterHazards.jl
@@ -140,8 +140,10 @@ single state and thus should add to 1 with all entries non-negative. Note this
 assumes the inputs are well formed, there are no bounds or sanity checks.
 """
 function covariance(C::T, P::T, n::AbstractVector{Int64})::T where T<:AbstractMatrix{Float64}
-    T = P*Diagonal(n)*P'
-    P*C*P' - 2 * Diagonal(T)
+    T = P*(n'.*P')
+    i = [1:(1 + size(T, 1)):length(T)...]
+    T[i] .= (P*n) .- T[i]
+    P*C*P' + T
 end
 
 end
