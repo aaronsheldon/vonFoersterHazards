93477e51a99c5f93de1d3bc3a08f8b94602e422d
Aaron Sheldon <aaronsheldon@hushmail.com>
2020-04-20 12:11:10 -0600

    Starting to flush out #4

--- src/CommunicableDisease.jl
+++ src/CommunicableDisease.jl
@@ -5,6 +5,13 @@ using vonFoersterHazards
 function gompertzrate end
 function scatterrate end
 function makehamrate end
+function infectionrate end
+function dischargerate end
+function recoveryrate end
+function hospitalizationrate end
+function ageingmortality end
+function hospitalmortality end
+function infectionmortality end
 function hazardrate end
 function hazardrate end
 function birthrate end
