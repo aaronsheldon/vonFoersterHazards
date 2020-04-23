55dc4b4810535c539b2fd8f5b19d45b0f010fab0
Aaron Sheldon <aaronsheldon@hushmail.com>
2020-04-23 14:32:26 -0600

    Push all type checking down to parameterized immutables, where it should happen #5

--- src/vonFoersterHazards.jl
+++ src/vonFoersterHazards.jl
@@ -14,9 +14,10 @@ import Base.iterate,
        Base.getindex,
        Base.setindex!,
        Base.size
+
 export randomtruncate,
        conservesum!,
-       covariance,
+       covariance!,
        cohort,
        population
        evolve,
@@ -24,7 +25,7 @@ export randomtruncate,
        birthrate
 
 """
-    cohort(age, occupancy, covariance, elapsed)
+    cohort(elapsed, age, occupancy, covariance)
 
 Return type for indexing into the population. Container for the state occupancies
 of a specific age group, and the covariances between the occupancies.
@@ -62,7 +63,6 @@ struct population{
     covariances::T
 end
 population(a, c) = population(0.0, a, c, zeros(Float64, size(c, 1), size(c, 2), size(c, 2)))
-
 Base.eltype(::Type{population{Q, R{U}, S{V}, T{W}}}) = cohort{
     Q, 
     U, 
@@ -91,7 +91,6 @@ struct evolve{S<:population, T<:Real}
     size::T
     count::Int64
 end
-
 Base.eltype(::Type{evolve{S, T}}) = S
 Base.length(E::evolve) = E.count
 Base.size(E::evolve, d=1) = ((d==1) ? length(E) : 1)
@@ -108,7 +107,7 @@ function Base.iterate(E::evolve)
     (convert(eltype(E.initial.cohorts), 0) <= minimum(E.initial.cohorts)) ||
         throw(DomainError(E.initial.cohorts, "cohorts must be non-negative."))
     
-     (minimum(E.initial.covariances) == convert(eltype(E.initial.covariances), 0) == maximum(E.initial.covariances)) ||
+    (minimum(E.initial.covariances) == convert(eltype(E.initial.covariances), 0) == maximum(E.initial.covariances)) ||
         throw(DomainError(E.initial.covariances, "covariances must be zero."))   
 
     H = hazardrate(E.initial)
@@ -125,42 +124,63 @@ function Base.iterate(E::evolve)
     (bl  == cw == dl == dw == el == ew == fw == fh) ||
         throw(DimensionMismatch("states in the covariances, birth rate, extensize hazard rate, intensize hazard rate, and cohorts must be equal."))
     
-    (E.initial, 0)
+    (E.initial, (0, E.initial))
 end
-function Base.iterate(E::evolve, step::Int64)
-    if step > E.count
+function Base.iterate(E::evolve, S)
+    if S[1] > E.count
         nothing
     else
         
         # One time computation of the extensize birth rate
-        b = birthrate(E.initial)
+        b = birthrate(S[2])
         
         # One time computation of the extensive hazard rate
-        H = hazardrate(E.intial)
+        H = hazardrate(S[2])
         
         # Curried transition function
-        t(a, n) = conservesum!(randomtruncate.(exp(-E.size * hazardrate(a, H)) * n), sum(n))
+        function t(c)
+            P = exp(-E.size * hazardrate(a, H))
+            r = cohort(
+                c.elapsed + E.size,
+                c.age + E.size,
+                conservesum!(randomtruncate.(P * c.occupancy), sum(c.occupancy)),
+                c.covariance
+            )
+            covariance!(r.covariance, P, r.occupancy)
+            r
+        end
         
-        # Compute the transitions within each cohort from the intensive hazard rate
-        E.population .= t.(E.ages, E.population)
-        E.ages .= E.ages .+ E.size
-        E.elapsed = E.elapsed + E.size
+        # Compute the transitions within each cohort
+        S[2] .= t.(S[2])
         
         # Youngest cohort is less than 1 year old, add births to youngest cohort
-        if E.ages[end] < 1.0 then
-            E.population[end] = E.population[end] + b
+        if S[1].ages[end] < 1.0 then
+            R = population(
+                S[2] + E.size,
+                S[2].ages,
+                S[2].cohorts,
+                S[2].covariances
+            )
+            R.cohorts[end, :] .= R.cohorts[end, :] .+ b'
             
         # Youngest cohort is more than 1 year old, generate a new youngest cohort
         else
-            push!(E.ages, 0.0)
-            push!(E.population, b)
+            R = population(
+                S[2] + E.size,
+                S[2].ages,
+                [S[2].cohorts; b'],
+                S[2].covariances
+            )
+            push!(R.ages, convert(eltype(R.ages), 0))
         end
-        (E.initial, step + 1)
+        
+        # Send
+        (R, (1 + S[1], R))
     end
 end
 
 """
-    birthrate(E)
+    birthrate(P)
 
 Stub function to be overloaded in implementation. Compute the extensive
 birth rate vector from the population P.
@@ -168,7 +188,7 @@ birth rate vector from the population P.
 function birthrate(P::population) end
 
 """
-    hazardrate(E)
+    hazardrate(P)
 
 Stub function to be overloaded in implementation. Compute the extensive
 hazard rate matrix from the population P.
@@ -179,7 +199,7 @@ function hazardrate(P::population) end
     hazardrate(a, H)
 
 Stub function to be overloaded in implementation. Compute the intensive
-hazard rate matrix from the extensive hazard rate matrix and a given age a.
+hazard rate matrix from the extensive hazard rate matrix H and a given age a.
 """
 function hazardrate(a, H) end
 
@@ -190,7 +210,7 @@ Randomly return the floor or the ceiling by comparing the fractional part of the
 number to a sample from the uniform distribution on [0,1). If the fraction is
 greater than the random sample return the ceiling otherwise return the floor.
 """
-function randomtruncate(x::Float64)::Float64
+function randomtruncate(x)
     y = convert(Int64, trunc(x))
     y + convert(Int64, rand() < (x-y))
 end
@@ -198,10 +218,10 @@ end
 """
     conservesum!(A, a)
 
-In place enforcement that the sum of the columns of A equals the a. Note this 
+In place enforcement that the sum of the columns of A equals a. Note this 
 assumes the inputs are well formed, there are no bounds or sanity checks.
 """
-function conservesum!(A::T, a::Int64)::T where T<:AbstractVector{Int64}
+function conservesum!(A, a)
     d = a - sum(A)
     I = sortperm(A, rev=(d<0))
     A[I] .= A[I] .+ [sign(d) .* ones(Int64, abs(d)) ; zeros(Int64, length(A)-abs(d))]
@@ -209,18 +229,19 @@ function conservesum!(A::T, a::Int64)::T where T<:AbstractVector{Int64}
 end
 
 """
-    covariance(C, P, n)
+    covariance!(C, P, n)
 
 Update of the covariances C from the transition probabilities P and the previous
 state n. Each column of P is assumed to represent the exit  probabilities from a
 single state and thus should add to 1 with all entries non-negative. Note this
 assumes the inputs are well formed, there are no bounds or sanity checks.
 """
-function covariance(C::T, P::T, n::AbstractVector{Int64})::T where T<:AbstractMatrix{Float64}
+function covariance!(C, P, n)
     U = (P.*n')*P'
     i = [1:(1 + size(U, 1)):length(U)...]
     U[i] .= (P*n) .- U[i]
-    P*C*P' + U
+    C .= P*C*P' .+ U
+    C
 end
 
 end
