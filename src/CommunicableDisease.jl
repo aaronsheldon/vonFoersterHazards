5fbf230e304bb367bfef357753a35fa38a13d3c8
Aaron Sheldon <aaronsheldon@hushmail.com>
2020-04-28 11:46:44 -0600

    - clean up parameter table #2 - add more self documenting constants to demonstration #6

--- src/CommunicableDisease.jl
+++ src/CommunicableDisease.jl
@@ -11,30 +11,61 @@ module CommunicableDisease
 
 using vonFoersterHazards
 
-const ontology = [ "Person"    "Susceptible";
-                   "Person"    "Infected";
-                   "Person"    "Hospitalized";
-                   "Person"    "Recovered";
-                   "Person"    "Discharged";
-                   "Person"    "Ageing Fatalities";
-                   "Person"    "Infected Fatalities";
-                   "Person"    "Hospitalized Fatalities";
-                   "Acute Bed" "Available";
-                   "Acute Bed" "Occupied";
-                   "Acute Bed" "Refractory" ]
+export stateontology,
+       statetransitions,
+       parameterontology,
+       parameterestimates
 
-const transitions = [  1  2;
-                       1  6;
-                       2  3;
-                       2  4;
-                       2  7;
-                       3  5;
-                       3  8;
-                       4  6;
-                       5  6;
-                       9 10;
-                      10 11;
-                      11  9 ]
+const stateontology = [ "Person"    "Susceptible";
+                        "Person"    "Infected";
+                        "Person"    "Hospitalized";
+                        "Person"    "Recovered";
+                        "Person"    "Discharged";
+                        "Person"    "Ageing Fatalities";
+                        "Person"    "Infected Fatalities";
+                        "Person"    "Hospitalized Fatalities";
+                        "Acute Bed" "Available";
+                        "Acute Bed" "Occupied";
+                        "Acute Bed" "Refractory" ]
+
+const statetransitions = [  1  2;
+                            1  6;
+                            2  3;
+                            2  4;
+                            2  7;
+                            3  5;
+                            3  8;
+                            4  6;
+                            5  6;
+                            9 10;
+                           10 11;
+                           11  9 ]
+
+const parameterontology = [ "Endogenous" "Background Mortality" "Base Rate"        "Deaths per Person Year";
+                            "Endogenous" "Background Mortality" "Doubling Time"    "per Age Year";
+                            "Endogenous" "Background Admission" "Base Rate"        "Admissions per Person Year";
+                            "Endogenous" "Background Admission" "Doubling Time"    "per Age Year";
+                            "Endogenous" "Background Discharge" "Base Rate"        "Discharges per Person Day";
+                            "Endogenous" "Background Discharge" "Half Life"        "per Age Year";
+                            "Endogenous" "Puberty"              "Doubling Time"    "per Age Year";
+                            "Endogenous" "Puberty"              "Mid-Point"        "Age Year";
+                            "Endogenous" "Infection"            "Acceleration"     "Dimensionless";
+                            "Exogenous"  "Infection"            "Contact Rate"     "Contact per Person Day";
+                            "Exogenous"  "Infection"            "Supression Ratio" "Dimensionless";
+                            "Endogenous" "Acute Bed"            "Refraction Rate"  "Availabilities per Bed Day"; ]
+
+const parameterestimates = [ 1.0/32000.0;
+                                     7.0;
+                               1.0/320.0;
+                                    14.0;
+                                1.0/16.0;
+                                    49.0; 
+                                     2.0;
+                                    16.0;
+                                     1.3;
+                                     0.1;
+                                     0.3;
+                                     2.0; ]
 
 function gompertzrate end
 function makehamrate end
