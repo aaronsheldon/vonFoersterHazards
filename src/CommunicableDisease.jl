b079afe6b5c0b50387e0a44f68f9d98e221ee6d3
Aaron Sheldon <aaronsheldon@hushmail.com>
2020-05-05 12:53:03 -0600

    More self documentation of the demonstration model #4

--- src/CommunicableDisease.jl
+++ src/CommunicableDisease.jl
@@ -12,9 +12,12 @@ module CommunicableDisease
 using vonFoersterHazards
 
 export stateontology,
+       processontology,
        statetransitions,
        parameterontology,
        parameterestimates,
+       configurationontology,
+       configurationvalues,
        hazardrate,
        scatterrate,
        birthrate,
@@ -28,38 +31,52 @@ const stateontology = ( head = [ "Noun"      "Adjective" ],
                                  "Person"    "Discharged";
                                  "Person"    "Ageing Fatality";
                                  "Person"    "Infected Fatality";
-                                 "Person"    "Hospitalized Fatality";
+                                 "Person"    "Hospital Fatality";
                                  "Acute Bed" "Available";
                                  "Acute Bed" "Occupied";
                                  "Acute Bed" "Refractory" ] )
 
-const statetransitions = ( head = [ "Source" "Target" ],
-                           body = [        1        2;
-                                           1        6;
-                                           2        3;
-                                           2        4;
-                                           2        7;
-                                           3        5;
-                                           3        8;
-                                           4        6;
-                                           5        6;
-                                           9       10;
-                                          10       11;
-                                          11        9 ] )
+const processontology = ( head = [ "Process"              "Source Noun" "Source Adjective" "Target Noun" "Target Adjective"],
+                          body = [ "Background Mortality" "Person"      "Susceptible"      "Person"      "Ageing Fatality"; 
+                                   "Background Mortality" "Person"      "Recovered"        "Person"      "Ageing Fatality";
+                                   "Background Mortality" "Person"      "Discharged"       "Person"      "Ageing Fatality";
+                                   "Infected Mortality"   "Person"      "Infected"         "Person"      "Infected Fatality"; 
+                                   "Infected Mortality"   "Person"      "Hospitalized"     "Person"      "Hospital Fatality"; 
+                                   "Admission"            "Person"      "Infected"         "Person"      "Hospitalized"; 
+                                   "Discharge & Recovery" "Person"      "Infected"         "Person"      "Recovered"; 
+                                   "Discharge & Recovery" "Person"      "Hospitalized"     "Person"      "Discharged";
+                                   "Infection"            "Person"      "Susceptible"      "Person"      "Infected"; 
+                                   "Bed Allocate"         "Acute Bed"   "Available"        "Acute Bed"   "Occupied"; 
+                                   "Bed Release"          "Acute Bed"   "Occupied"         "Acute Bed"   "Refractory";
+                                   "Bed Recover"          "Acute Bed"   "Refractory"       "Acute Bed"   "Available" ] )
 
-const parameterontology = ( head = [ "Decomposition" "Process"              "Metric"           "Units" ],
-                            body = [ "Endogenous"    "Background Mortality" "Base Rate"        "Deaths per Person Year";
-                                     "Endogenous"    "Background Mortality" "Doubling Time"    "per Age Year";
-                                     "Endogenous"    "Background Admission" "Base Rate"        "Admissions per Person Year";
-                                     "Endogenous"    "Background Admission" "Doubling Time"    "per Age Year";
-                                     "Endogenous"    "Background Discharge" "Base Rate"        "Discharges per Person Day";
-                                     "Endogenous"    "Background Discharge" "Half Life"        "per Age Year";
-                                     "Endogenous"    "Puberty"              "Doubling Time"    "per Age Year";
-                                     "Endogenous"    "Puberty"              "Mid-Point"        "Age Year";
-                                     "Endogenous"    "Infection"            "Acceleration"     "Dimensionless";
-                                     "Exogenous"     "Infection"            "Contact Rate"     "Contacts per Person Day";
-                                     "Exogenous"     "Infection"            "Supression Ratio" "Dimensionless";
-                                     "Endogenous"    "Acute Bed"            "Refraction Rate"  "Availabilities per Bed Day" ] )
+const statetransitions = ( head = [ "Source" "Target" "Process" ],
+                           body = [        1        2         5;
+                                           1        6         1;
+                                           2        3         3;
+                                           2        4         4;
+                                           2        7         2;
+                                           3        5         4;
+                                           3        8         2;
+                                           4        6         1;
+                                           5        6         1;
+                                           9       10         6;
+                                          10       11         7;
+                                          11        9         8 ] )
+
+const parameterontology = ( head = [ "Decomposition" "Process"              "Metric"            "Units" ],
+                            body = [ "Endogenous"    "Background Mortality" "Base Rate"         "Deaths per Person Year";
+                                     "Endogenous"    "Background Mortality" "Doubling Time"     "per Age Year";
+                                     "Endogenous"    "Background Admission" "Base Rate"         "Admissions per Person Year";
+                                     "Endogenous"    "Background Admission" "Doubling Time"     "per Age Year";
+                                     "Endogenous"    "Background Discharge" "Base Rate"         "Discharges per Person Day";
+                                     "Endogenous"    "Background Discharge" "Half Life"         "per Age Year";
+                                     "Endogenous"    "Puberty"              "Doubling Time"     "per Age Year";
+                                     "Endogenous"    "Puberty"              "Mid-Point"         "Age Year";
+                                     "Endogenous"    "Infection"            "Acceleration"      "Dimensionless";
+                                     "Exogenous"     "Infection"            "Contact Rate"      "Contacts per Person Day";
+                                     "Exogenous"     "Infection"            "Suppression Ratio" "Dimensionless";
+                                     "Endogenous"    "Acute Bed"            "Refraction Rate"   "Availabilities per Bed Day" ] )
 
 const parameterestimates = ( head = [     "Value" ], 
                              body = [ 1.0/32000.0;
@@ -103,20 +120,20 @@ end
 """
     hazardrate(p)
 
-The hazard rate matrix is spectrally decomposed into 6 age-Eigen function matrices,
+The hazard rate matrix is spectrally decomposed into 8 age-Eigen function matrices,
 each mapping 11 states to 11 states.
 """
 function hazardrate(p::population{Int64, Vector{Int64}, Matrix{Int64}, Array{Float64, 3}})
-    zeros(Float64, 6, 11,11)
+    zeros(Float64, 8, 11,11)
 end
 
 """
     scatterrate(a)
 
-The scattering rate decomposes the hazard rate into 6 age scattering cross sections.
+The scattering rate decomposes the hazard rate into 8 age scattering cross sections.
 """
 function scatterrate(a::Int64)
-    zeros(Float64, 6)
+    zeros(Float64, 8)
 end
 
 """
